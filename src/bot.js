8fa4e8347c048eb7d9560425cfb7922c09d2016c
mahessh77melo <magesh.lbj23@gmail.com>
2020-12-05 10:53:27 +0530

    movie query added

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,12 @@ const { Client, ClientPresence } = require("discord.js");
 require("dotenv").config();
 
 const client = new Client();
+const axios = require("axios");
+
+const myAxios = axios.create({
+	baseURL: "https://api.themoviedb.org/3",
+});
+
 client.login(process.env.DISCORD_BOT_TOKEN);
 client.on("ready", () =>
 	console.log(`${client.user.tag} is online and ready to go!!`)
@@ -19,8 +25,42 @@ I am still in the development phase. Try giving commands to me.. My commands sho
 	} else if (message.content.startsWith(botCommandStarter)) {
 		// main commands
 		const command = message.content.split(botCommandStarter).slice(1).join("");
-		message.channel.send(`You have given the **${command}** command to me!`);
+		// message.channel.send(`You have given the **${command}** command to me!`);
+		// searching for movie or a tv show
+		if (command.toLowerCase().startsWith("movie"))
+			search("movie", command.split(" ").slice(1).join(" "), message);
+		if (command.toLowerCase().startsWith("tv"))
+			search("tv", command.split(" ").slice(1).join(" "), message);
 	} else return;
 };
+
+async function search(media, name, message) {
+	try {
+		const res = await myAxios.get(
+			`/search/${media}?api_key=${process.env.TMDB_KEY}&query=${name
+				.split(" ")
+				.join("+")}`
+		);
+		const mainResult = res.data.results[0];
+		if (mainResult) {
+			message.reply(`
+Name of the ${media === "tv" ? "TV Show" : media} : **${
+				mainResult.name || mainResult.title
+			}**
+
+${process.env.IMG_LINK}${mainResult.poster_path}
+
+Overview : ${mainResult.overview}
+
+Released On : **${
+				mainResult.first_air_date || mainResult.release_date + "  "
+			}**`);
+		} else {
+			message.reply(`No ${media === "tv" ? "TV Show" : media} of that sort.`);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 client.on("message", interact);
